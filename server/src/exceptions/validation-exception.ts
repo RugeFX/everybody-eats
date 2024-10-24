7032a54bd01ca7b250b240c6d2ac3ca97d6913d8
rugefx <zackfxg@gmail.com>
2024-10-25 00:26:06 +0700

    chore (server): remove unused exceptions

--- server/src/exceptions/validation-exception.ts
+++
@@ -1,9 +0,0 @@
-export class ValidationException extends Error {
-	public errors: Record<string, unknown>;
-
-	constructor(message: string, errors: Record<string, unknown>) {
-		super(message);
-		this.name = "ValidationException";
-		this.errors = errors;
-	}
-}
